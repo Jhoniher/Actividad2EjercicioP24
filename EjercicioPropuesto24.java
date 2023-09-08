25c1bde1481448e6c44fc7c31859d3fae44b4ce7
Jhony Alpala <jhoniher95@gmail.com>
2023-09-08 01:29:05 -0500

    Ejercicio finalizado

--- EjercicioPropuesto24.java
+++ EjercicioPropuesto24.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class EjercicioPropuesto24 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
-        double pesoA, pesoB, pesoC;
+        double pesoA, pesoB, pesoC; //Declaracion de variables
         System.out.println("Ingrese peso de esfera A: ");
         pesoA=entrada.nextDouble();
         System.out.println("Ingrese peso de esfera B: ");
         pesoB=entrada.nextDouble();
         System.out.println("Ingrese peso de esfera C: ");
         pesoC=entrada.nextDouble();
-        if(pesoA>pesoB && pesoA>pesoC){
+        if(pesoA>pesoB && pesoA>pesoC){ //Se comparan pesos
             System.out.println("La esfera A es la de mayor peso");
         }else if(pesoB>pesoA && pesoB>pesoC){
             System.out.println("La esfera B es la de mayor peso");
