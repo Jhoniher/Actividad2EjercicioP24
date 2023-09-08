163897f3ab1c182da3e542ec73baa3fedda6b192
Jhony Alpala <jhoniher95@gmail.com>
2023-09-08 01:28:26 -0500

    Codigo completado

--- EjercicioPropuesto24.java
+++ EjercicioPropuesto24.java
@@ -1,2 +1,21 @@
+//Ejercicio, mayor de 3 esferas
+import java.util.Scanner;
 public class EjercicioPropuesto24 {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        double pesoA, pesoB, pesoC;
+        System.out.println("Ingrese peso de esfera A: ");
+        pesoA=entrada.nextDouble();
+        System.out.println("Ingrese peso de esfera B: ");
+        pesoB=entrada.nextDouble();
+        System.out.println("Ingrese peso de esfera C: ");
+        pesoC=entrada.nextDouble();
+        if(pesoA>pesoB && pesoA>pesoC){
+            System.out.println("La esfera A es la de mayor peso");
+        }else if(pesoB>pesoA && pesoB>pesoC){
+            System.out.println("La esfera B es la de mayor peso");
+        }else{
+            System.out.println("La esfera C es la de mayor peso");
+        }
+    }
 }
